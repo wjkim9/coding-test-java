7d32db441c75875b3f81d7355b0228a62445ed19
wjkim9 <kin9216@gmail.com>
2025-07-20 22:16:14 +0900

    '20250720'

---
+++ src/com/test/coding/stackqueuedeck/CT11866.java
@@ -0,0 +1,47 @@
+package com.test.coding.stackqueuedeck;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class CT11866 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        Deque<Integer> queue = new ArrayDeque<>();
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        for (int i=1; i<=n; i++) {
+        	queue.add(i);
+        }
+        
+        StringBuilder sb = new StringBuilder("<");
+        
+        while (queue.size() > 1) {
+        	for (int i=0; i<k-1; i++) {
+        		queue.add(queue.poll());
+        	}
+        	
+        	sb.append(queue.poll())
+        		.append(", ");
+        }
+        
+        sb.append(queue.poll()).append(">");
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
---
+++ src/com/test/coding/stackqueuedeck/CT2164.java
@@ -0,0 +1,30 @@
+package com.test.coding.stackqueuedeck;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CT2164 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        
+        Deque<Integer> queue = new ArrayDeque<>();
+        
+        for (int i=1; i<=n; i++) {
+        	queue.add(i);
+        }
+        
+        for (int i=0; i<n-1; i++) {
+        	queue.poll();
+        	queue.add(queue.poll());
+        }
+        
+        System.out.println(queue.poll());
+    }
+}
