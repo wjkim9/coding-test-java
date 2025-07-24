30a7d5bca41d22a2cac0847ae1d23f4e41af3ba3
wjkim9 <kin9216@gmail.com>
2025-07-24 23:48:44 +0900

    '20250724'

---
+++ src/com/test/coding/combinatorics/CT10872.java
@@ -0,0 +1,24 @@
+
+package com.test.coding.combinatorics;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class CT10872 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		bw.write((int)Math.pow(2, n) + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
