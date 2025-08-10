3910cec073bf1b1422abe8cd3b7d536f58c0a7a6
wjkim9 <kin9216@gmail.com>
2025-08-10 22:35:08 +0900

    '2025-08-10'

--- src/com/test/coding/backtracking/CT15649.java
+++ src/com/test/coding/backtracking/CT15649.java
@@ -1,30 +1,46 @@
 package com.test.coding.backtracking;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class CT15649 {
-	
+
+    static int n, m;
+    static int[] pick;
+    static StringBuilder sb = new StringBuilder(1 << 20); // 대략 1MB 버퍼
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        
-        
-        
-        
-        
-        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        pick = new int[m];
+        dfs(0, 0); // depth=0, used=0(아무것도 안 쓴 상태)
+
+        bw.write(sb.toString());
         bw.flush();
         br.close();
         bw.close();
     }
 
+    // used: 1<<(num-1) 비트가 켜져 있으면 해당 숫자 사용됨
+    static void dfs(int depth, int used) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(pick[i]);
+                if (i + 1 < m) sb.append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int num = 1; num <= n; num++) {
+            int bit = 1 << (num - 1);
+            if ((used & bit) != 0) continue;  // 이미 사용한 수
+            pick[depth] = num;
+            dfs(depth + 1, used | bit);
+        }
+    }
 }
