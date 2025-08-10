48694cef203791e50c72e7811860b9fe059999f8
wjkim9 <kin9216@gmail.com>
2025-08-10 12:24:24 +0900

    '2025-08-10'

--- src/com/test/coding/dynamicpro1/CT1904.java
+++ src/com/test/coding/dynamicpro1/CT1904.java
@@ -7,45 +7,27 @@ public class CT1904 {
 	
 	 static List<List<int[]>> result = new ArrayList<>();
 	
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        
-//        long[] dp = new long[n + 1];
-//
-//        dp[0] = 1;
-//        dp[1] = 1;
-//
-//        for (int i = 2; i <= n; i++) {
-//            dp[i] = dp[i - 1] + dp[i - 2];
-//        }
-        
-        List<int[]> current = new ArrayList<>();
-        backtrack(current, 0, n);
-        
-        bw.write(result.size() + "");
-        
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    
-    static void backtrack(List<int[]> current, int currentLength, int targetLength) {
-        if (currentLength == targetLength) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-        if (currentLength > targetLength) return;
+	 public static void main(String[] args) throws Exception {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int n = Integer.parseInt(br.readLine());
 
-        current.add(new int[]{1});
-        backtrack(current, currentLength + 1, targetLength);
-        current.remove(current.size() - 1);
+	        if (n == 1) {
+	            System.out.println(1);
+	            return;
+	        }
+	        if (n == 2) {
+	            System.out.println(2);
+	            return;
+	        }
 
-        current.add(new int[]{0, 0});
-        backtrack(current, currentLength + 2, targetLength);
-        current.remove(current.size() - 1);
-    }
+	        int a = 1;
+	        int b = 2;
+	        for (int i = 3; i <= n; i++) {
+	            int c = (a + b) % 15746;
+	            a = b;
+	            b = c;
+	        }
+	        System.out.println(b);
+	    }
 
 }
