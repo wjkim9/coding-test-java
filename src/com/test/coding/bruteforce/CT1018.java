becf0e87c5e60d7243920ae0a1e9a1305703ed2b
wjkim9 <kin9216@gmail.com>
2025-08-24 11:51:54 +0900

    '2025-08-24'

--- src/com/test/coding/bruteforce/CT1018.java
+++ src/com/test/coding/bruteforce/CT1018.java
@@ -1,61 +1,43 @@
 package com.test.coding.bruteforce;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class CT1018 {
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		
-		int min = 65;
-		
-		while (true) {
-			
-			
-		
-			int color = 0;
-			int cnt = 0;
-			
-			for (int i=0; i<8; i++) {
-				String row = br.readLine();
-					
-				for (int j=0; j<8; j++) {
-					
-					switch (color) {
-						case 0: 
-							if (row.charAt(j) != 'B') {
-								cnt++;
-							}
-							color = 1;
-							break;
-						case 1:
-							if (row.charAt(j) != 'W') {
-								cnt++;
-							}
-							color = 0;
-							break;
-					}
-				}
-			}
-			
-			if (64 - cnt < cnt) {
-				cnt = 64 - cnt;
-			}
-			
-			if (cnt < min) {
-				min = cnt;
-			}
-		
-		}
-		
-		
-	}
+        char[][] a = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) a[i][j] = s.charAt(j); // 'W' or 'B'
+        }
+
+        int best = 64; // 최대 8x8 전체 칠하기
+        for (int i = 0; i + 7 < N; i++) {
+            for (int j = 0; j + 7 < M; j++) {
+                int diffW = 0, diffB = 0;
+                for (int r = i; r < i + 8; r++) {
+                    for (int c = j; c < j + 8; c++) {
+                        boolean sameParity = ((r - i) + (c - j)) % 2 == 0;
+                        char expectedW = sameParity ? 'W' : 'B';
+                        char expectedB = sameParity ? 'B' : 'W';
+                        if (a[r][c] != expectedW) diffW++;
+                        if (a[r][c] != expectedB) diffB++;
+                    }
+                }
+                int cur = Math.min(diffW, diffB);
+                if (cur < best) best = cur;
+                if (best == 0) { // 조기 종료(이미 완벽한 8x8 발견)
+                    System.out.println(0);
+                    return;
+                }
+            }
+        }
+        System.out.println(best);
+    }
 }
